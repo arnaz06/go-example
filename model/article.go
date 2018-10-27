3f459001629857ed8487e6f7f35a611c1b6890fa
sikun <arnazadiputra@gmail.com>
2018-10-27 18:39:16 +0700

    fixing pingConnection

--- model/article.go
+++ model/article.go
@@ -11,22 +11,21 @@ var err error
 
 type Article struct{
 	gorm.Model
-	id int64
-	Title string
-	Content string
-	Thumbnail string
+	id int64	`json:"Id"`
+	Title string	`json:"Title"`
+	Content string	`json:"Content"`
+	Thumbnail string	`json:"Thumbnail"`
 
 }
 
 func InitialArticleMigration(){
-	db, err= gorm.Open("mysql", "root:laskar45@/articles")
-
-	if err != nil{
-		fmt.Println(err.Error())
-		panic("Faild to Connect to DB")
-	}
+		db, err= gorm.Open("mysql", "root:laskar45@/articles")
 	
-	defer db.Close()
+		if err != nil{
+			fmt.Println(err.Error())
+			panic("Faild to Connect to DB")
+		}
 
+	defer db.Close()
 	db.AutoMigrate(&Article{})
 }
